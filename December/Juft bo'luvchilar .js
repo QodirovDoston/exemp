defad887efb25f677e12766d5302f317e6f32803
QodirovDoston <qodirovdoston806@gmail.com>
2022-12-26 20:40:20 +0500

    dksndjf

--- December/Juft bo'luvchilar .js
+++ December/Juft bo'luvchilar .js
@@ -1,4 +1,13 @@
-let a = 6 
-for (let i = 0; i <= a.lenth; i++){
+// let a = 6 
+// for (let i = 0; i <= a.lenth; i++){
 
+// }
+
+function divisorSum( n)
+{
+    sum = 0;
+    for (let i = 1; i <= n; ++i)
+        sum += (n / i) * i;
+    return sum;
 }
+ 
